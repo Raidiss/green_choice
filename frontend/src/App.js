3af5d76a210abadfe73512b2b5e6f43c65e3e693
Raidiss <raidiss@gmail.com>
2019-07-24 03:25:41 -0400

    adding initial project

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      
+        <div className="container">
+          <h2>Hello World!!</h2>
+        </div>
+      
+    );
+  }
+}
+export default App;
