cc24800a00c1a72b3f051cae861d2f101ede9fbf
Hector Martinez <hmartinez@venuetize.com>
2019-07-24 17:36:05 -0400

    adding a couple of components

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    constructor(props) {
+        super(props);
+        state = {};
+    }
+    
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Product;
---
+++ frontend/src/components/Products.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Products extends Component {
+    constructor(props) {
+        super(props);
+        state = {};
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Products;
